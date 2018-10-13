294146f9a3a7d3630e416b43631862c6f78bd5e5
duncanidaho14 <elhadibeddarem@gmail.com>
2018-10-13 10:19:33 +0200

    position de la vidéo de la page principale

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import $ from 'jquery';
-import "bootstrap";
+import 'bootstrap';
 
 $(document).ready(function() {
-   console.log(document) 
+   console.log(document);
+    let courses: any = {};
+    let xmlhttp: any = new XMLHttpRequest();
+    xmlhttp.open("GET", "./pokedex.json", true);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            courses = console.log(JSON.parse(xmlhttp.responseText));
+        }
+    };
+    xmlhttp.send(null);
+ 
 })
-let courses : any = {};
-let xmlhttp : any = new XMLHttpRequest();
-xmlhttp.open("GET", "./pokedex.json", true);
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        courses = console.log(JSON.parse(xmlhttp.responseText));
-    }
-};
-xmlhttp.send(null);
